9e2241d7085450d2787d7743fc679d921a5999e0
Ulla <ullavansprang@gmail.com>
2022-09-15 17:08:11 +0200

    First e2e cypress test

---
+++ cypress/e2e/1-home/homepage.cy.js
@@ -0,0 +1,28 @@
+describe("homepage", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:5500/index.html");
+  });
+
+  it("displays the title", () => {
+    cy.get(".header-container h1").should("have.text", "Find your film");
+  });
+
+  it("displays the default empty state", () => {
+    cy.get(".start-exploring-text").should("have.text", "Start exploring");
+  });
+
+  it("can search for the movie Amelie", () => {
+    cy.get(".search-input").type("Amelie{enter}");
+
+    cy.get(".movie-result").should("have.length", "10");
+
+    cy.get(".movie-result:nth-child(1) .movie-title a").should(
+      "have.text",
+      "Amélie"
+    );
+
+    cy.get(".movie-result:nth-child(1) .add-watchlist").click();
+
+    cy.get(".movie-result:nth-child(1) .remove-watchlist").should("exist");
+  });
+});
